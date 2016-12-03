742806dd6f63a384ec27c1422e7b69b334fb9d2c
Alexander <qsok3d@gmail.com>
2016-12-04 00:22:44 +0400

    add Calculate with javaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,9 @@
 package ru.job4j;
+/**
+ *Class Calculate решение задачи части 001 урок1.
+ *@author erokhov
+ *@since 02.12.2016
+**/
 
 public class Calculate{
  public static void main(String[] args){
