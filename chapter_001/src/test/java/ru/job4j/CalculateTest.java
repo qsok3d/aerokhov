77c9abf5ef4d090691a9b58270953fa08eda4f9a
Alexander <qsok3d@gmail.com>
2016-12-06 20:33:12 +0400

    add new Test

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,20 +7,20 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-*Test
+*Test.
 *
 *@author Alexander Erokhov (qsok3d@gmail.com)
 *@version $Id$
-*@snipe 0.1
+*@since 0.1
 */
 
-public class CalculateTest{
+public class CalculateTest {
 /**
 *Test add.
 */
 
 @Test
-public void whenAddOneToOneThenTwo(){
+public void whenAddOneToOneThenTwo() {
 	ByteArrayOutputStream out = new ByteArrayOutputStream();
 	System.setOut(new PrintStream(out));
 	Calculate.main(null);
