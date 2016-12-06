54ecc9885cdce42d4ea53ddd882c268de1cbb1eb
Alexander <qsok3d@gmail.com>
2016-12-06 20:31:20 +0400

    add main java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,12 +1,17 @@
 package ru.job4j;
+
 /**
  *Class Calculate решение задачи части 001 урок1.
  *@author erokhov
  *@since 02.12.2016
 **/
 
-public class Calculate{
- public static void main(String[] args){
+public class Calculate {
+	/**
+	*Main.
+	* @param args - args.
+	*/
+ public static void main(String[] args) {
   System.out.println("Hello World");
 }
 }
