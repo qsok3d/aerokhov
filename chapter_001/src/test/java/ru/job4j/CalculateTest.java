c34e5c5b54b6c3579aada73fba5258a1a1a3751d
Alexander <qsok3d@gmail.com>
2016-12-08 20:01:17 +0400

    add

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -24,6 +24,6 @@ public void whenAddOneToOneThenTwo() {
 	ByteArrayOutputStream out = new ByteArrayOutputStream();
 	System.setOut(new PrintStream(out));
 	Calculate.main(null);
-	assertThat(out.toString(), is("Hello World\r\n"));
+	assertThat(out.toString(), is(String.format("Hello World%s", System.getProperty("line.separator"))));
 }
 }
